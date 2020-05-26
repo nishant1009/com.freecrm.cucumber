fca081b80d58639523ccafebb79e4f809134e7d6
NishantG <Nishant_online@rediffmail.com>
2020-05-26 11:43:06 +0530

    NG: Updated Extent report location

--- com.freecrm.cucumber/src/test/java/runner/TestRunner.java
+++ com.freecrm.cucumber/src/test/java/runner/TestRunner.java
@@ -17,7 +17,7 @@ import cucumber.api.junit.Cucumber;
 		features="features",
 		glue={"stepDefination"},
 		format={"pretty", "html:target/Destination"},
-		plugin={"com.cucumber.listener.ExtentCucumberFormatter:target/FreeCrmTestReport.html "},
+		plugin={"com.cucumber.listener.ExtentCucumberFormatter:src/test/resources/FreeCrmTestReport.html "},
 		dryRun=false,
 		monochrome=true
 		)
